a92343bac849fe506c21d44e4aab43f62d129f68
rocky <rocky@163.com>
2018-07-16 16:14:13 +0800

    完成注册商家前端页面调整

--- apps/__init__.py
+++ apps/__init__.py
@@ -1,6 +1,7 @@
 """
 app的创建，这个文件叫做工厂模式
 建议以函数进行封装
+bootstrap插件跟其他插件没有任何冲突，可以最开始导入
 """
 from flask import Flask
 
@@ -17,6 +18,12 @@ def register_bp(app):
     app.register_blueprint(cms_bp)
 
 
+# bootstrap的插件注册
+def register_bs(app):
+    from flask_bootstrap import Bootstrap
+    Bootstrap(app)
+
+
 def create_app():
     app = Flask(__name__)
     # 配置app的一些跟数据库相关的配置项
@@ -24,6 +31,9 @@ def create_app():
 
     # 数据库对象的app绑定
     register_db(app)
+
+    # 注册bs
+    register_bs(app)
 
     # 把蓝图对象跟app进行绑定
     register_bp(app)
--- apps/cms/auth_view.py
+++ apps/cms/auth_view.py
@@ -1,13 +1,15 @@
+from flask import render_template
+
 from apps.cms import cms_bp
 
 
 # 创建首页
 @cms_bp.route('/', endpoint='index')
 def seller_index():
-    return "success"
+    return render_template('cms/index.html')
 
 
 # 创建注册页面
 @cms_bp.route('/register/', endpoint='register', methods=['GET', 'POST'])
 def seller_register():
-    return "1-success"
+    return render_template('cms/register.html')
